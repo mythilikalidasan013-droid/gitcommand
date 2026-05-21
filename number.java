649f1dcf8035d24d05ed0b183ee865b6b20f2744
Mythili k <mythilikalidasan013@gmail.com>
2026-05-21 14:59:25 +0530

    feat: add two numbers

---
+++ number.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class number {
+    public static void main (String [] args ) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number1");
+        int a = sc.nextInt();
+         System.out.print("Enter a number2");
+        int b = sc.nextInt();
+        System.out.println(a+b);
+    }
+}
